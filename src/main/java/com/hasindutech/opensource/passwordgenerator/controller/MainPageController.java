88258b6761be3384adc03692f75e86ee75c85de6
hasinduudara <hasindutechschool@gmail.com>
2024-12-02 11:40:12 +0530

    Create copy btn

--- src/main/java/com/hasindutech/opensource/passwordgenerator/controller/MainPageController.java
+++ src/main/java/com/hasindutech/opensource/passwordgenerator/controller/MainPageController.java
@@ -1,10 +1,18 @@
 package com.hasindutech.opensource.passwordgenerator.controller;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.util.Duration;
+
+import java.security.SecureRandom;
 
 public class MainPageController {
 
@@ -21,13 +29,47 @@ public class MainPageController {
     private ProgressBar pbar;
 
     @FXML
-    void btnCopyPasswordOnAction(ActionEvent event) {
+    void btnGeneratePasswordOnAction(ActionEvent event) {
+        pbar.setProgress(0);
+        Timeline timeline = new Timeline();
+        for (int i = 0; i <= 20; i++) {
+            double progress = i * 0.05;
+            timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(i * 0.1), e -> pbar.setProgress(progress)));
+        }
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(2.0), e -> {
+            String password = generatePassword(10);
+            lblPassword.setText(password);
+            pbar.setProgress(1);
+        }));
+        timeline.setCycleCount(1);
+        timeline.play();
+    }
 
+    private String generatePassword(int length) {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#&";
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            password.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return password.toString();
     }
 
     @FXML
-    void btnGeneratePasswordOnAction(ActionEvent event) {
+    void btnCopyPasswordOnAction(ActionEvent event) {
+        String password = lblPassword.getText();
+        if (!password.isEmpty()) {
+            Clipboard clipboard = Clipboard.getSystemClipboard();
+            ClipboardContent content = new ClipboardContent();
+            content.putString(password);
+            clipboard.setContent(content);
 
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Password Copied");
+            alert.setHeaderText(null);
+            alert.setContentText("The password has been copied to the clipboard.");
+            alert.showAndWait();
+        }
     }
 
 }
