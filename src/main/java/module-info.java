362ccf49f74104eb35c7e880641aded3fba876ad
hasinduudara <hasindutechschool@gmail.com>
2024-12-02 10:59:03 +0530

    Update model info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.hasindutech.opensource.passwordgenerator {
     requires javafx.fxml;
 
     opens com.hasindutech.opensource.passwordgenerator to javafx.fxml;
+    opens com.hasindutech.opensource.passwordgenerator.controller to javafx.fxml;
     exports com.hasindutech.opensource.passwordgenerator;
     exports com.hasindutech.opensource.passwordgenerator.controller;
 }
