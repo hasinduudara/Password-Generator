6028f93d67eaa46789385dc92463e450f3ece88c
hasinduudara <hasindutechschool@gmail.com>
2024-12-02 10:56:21 +0530

    Update UI

--- src/main/java/com/hasindutech/opensource/passwordgenerator/controller/MainPageController.java
+++ src/main/java/com/hasindutech/opensource/passwordgenerator/controller/MainPageController.java
@@ -1,4 +1,33 @@
 package com.hasindutech.opensource.passwordgenerator.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
 public class MainPageController {
+
+    @FXML
+    private Button btnCopyPassword;
+
+    @FXML
+    private Button btnGeneratePassword;
+
+    @FXML
+    private Label lblPassword;
+
+    @FXML
+    private ProgressBar pbar;
+
+    @FXML
+    void btnCopyPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGeneratePasswordOnAction(ActionEvent event) {
+
+    }
+
 }
